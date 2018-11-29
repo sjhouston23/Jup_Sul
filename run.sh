3d86dafc6b68e005ad442f681a3749f6f216817d
sjhouston23 <houston.stephen23@gmail.com>
2018-11-29 11:16:28 -0600

    Updated to run on new slurm environment

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
-#MSUB -q sixhour
-#MSUB -l nodes=1:ppn=1,walltime=06:00:00
-#MSUB -t 1-200
+#!/bin/bash                                                                                          
+#SBATCH --partition=sixhour
+#SBATCH --ntasks=1
+#SBATCH --mem=2gb
+#SBATCH --time=06:00:00
+#SBATCH --array=1-200
 
 R=$RANDOM
-./precip.x $R
+./oxy $R
 echo $R >> ./Output/Trials.dat
 exit 0
